7f7243359d76f564d891d624a367163295ba6302
Your Name <you@example.com>
2023-11-12 14:52:17 -0800

    All task done; ;

---
+++ str_functions2.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[j])
+	{
+		dest[j] = src[j];
+		j = j + 1;
+	}
+	dest[j] = 0;
+	return (dest);
+}
