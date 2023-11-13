1714dd08c61124f4bfdf94e98fbec9ac449fec42
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-11-13 13:04:48 -0800

    error fixed

--- envir_functions3.c
+++ envir_functions3.c
@@ -1,76 +1,92 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: structure
- *  Return: exits with a given exit status
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int _myexit(info_t *info)
+int _myenv(info_t *info)
 {
-	int retvall;
-
-	if (info->argv[1])
-	{
-		retvall = _erratoi(info->argv[1]);
-		if (retvall == -1)
-		{
-			info->status = 2;
-			print_error(info, "Ilegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
-	return (-2);
+	print_list_str(info->env);
+	return (0);
 }
 
 /**
- * _cd - changes the current directory of the process
- * @info: Structure
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
  *  Return: Always 0
  */
-int _cd(info_t *info)
+int _mysetenv(info_t *info)
 {
-	char *s, *directory, buff[1024];
-	int chdir_ret;
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of args\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
 
-	s = getcwd(buff, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
 	{
-		directory = _getenv(info, "HOME=");
-		if (!directory)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((directory = _getenv(info, "PWD=")) ? directory : "/");
-		else
-			chdir_ret = chdir(directory);
+		_eputs("Too few arguements.\n");
+		return (1);
 	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = chdir((directory = _getenv(info, "OLDPWD=")) ? directory : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buff, 1024));
-	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
 	return (0);
 }
